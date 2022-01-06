62b095643d44e66fddc5c768fb689bcd6ac2c6ec
faraz jahan manesh <farazebipax@gmail.com>
2022-01-06 07:43:46 -0800

    Add project files.

---
+++ ChannelExample/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Threading.Channels;
+
+var myChannel = Channel.CreateUnbounded<int>();
+var reader = myChannel.Reader;
+var writer = myChannel.Writer;
+var random = new Random();
+Task.Run(async () =>
+{
+    foreach (var i in Enumerable.Range(0, 10))
+    {
+        await writer.WriteAsync(random.Next(100));
+        await Task.Delay(1000);
+    }
+    writer.Complete();
+});
+
+await foreach (var item in reader.ReadAllAsync())
+{
+    Console.WriteLine("number is :" + item);
+}
+
+Console.WriteLine("--------------------The End-------------------------");
